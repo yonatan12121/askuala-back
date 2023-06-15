7a9f2aed59b20d9a52b5ec75cdb0c3f24e491176
beka-tegen <bekategene177@gmail.com>
2023-06-15 17:33:12 +0300

    modify

--- Server/Controller/controller.js
+++ Server/Controller/controller.js
@@ -386,25 +386,37 @@ exports.loginUser = async (req, res) => {
         });
       }
       if (role == "Admin") {
-        return res.json({ status: "ok", role,
-        id,
-        password,
-        fullName,
-        department, data: token });
+        return res.json({
+          status: "ok",
+          role,
+          id,
+          password,
+          fullName,
+          department,
+          data: token,
+        });
       }
       if (role == "SuperVisor") {
-        return res.json({ status: "ok", role,
-        id,
-        password,
-        fullName,
-        department, data: token });
+        return res.json({
+          status: "ok",
+          role,
+          id,
+          password,
+          fullName,
+          department,
+          data: token,
+        });
       }
       if (role == "Teacher") {
-        return res.json({ status: "ok", role,
-        id,
-        password,
-        fullName,
-        department, data: token });
+        return res.json({
+          status: "ok",
+          role,
+          id,
+          password,
+          fullName,
+          department,
+          data: token,
+        });
       }
     } else {
       return res.json({ error: "error" });
@@ -412,7 +424,6 @@ exports.loginUser = async (req, res) => {
   }
   res.json({ status: "error", error: "InvAlid Password" });
 };
-
 
 exports.storeTodo = (req, res) => {
   const { data } = req.body;
